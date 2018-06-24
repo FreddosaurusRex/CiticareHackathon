9c166fb1330ce34a74ca59f1699df41a2a5fdd6f
Andi Andreas <nexuister@gmail.com>
2018-06-23 23:06:21 -0400

    Add react-materialize

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,48 @@
 import React, { Component } from "react";
 import "./App.css";
+import {
+  Collapsible,
+  CollapsibleItem,
+  Row,
+  Col,
+  SideNav,
+  SideNavItem,
+  Button
+} from "react-materialize";
+
+require("dotenv").config();
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>andi</p>
+        <SideNav
+          fixed
+          trigger={<Button>SIDE NAV DEMO</Button>}
+          options={{ closeOnClick: true }}
+        >
+          <SideNavItem
+            userView
+            user={{
+              background: "https://react-materialize.github.io/img/office.jpg",
+              image: "https://react-materialize.github.io/img/yuna.jpg",
+              name: "John Doe",
+              email: "jdandturk@gmail.com"
+            }}
+          />
+          <SideNavItem href="#!icon" icon="cloud">
+            First Link With Icon
+          </SideNavItem>
+          <SideNavItem href="#!second">Second Link</SideNavItem>
+          <SideNavItem divider />
+          <SideNavItem subheader>Subheader</SideNavItem>
+          <SideNavItem waves href="#!third">
+            Third Link With Waves
+          </SideNavItem>
+        </SideNav>
+        <Row style={{ paddingLeft: "320px" }}>
+          <p>andi {process.env.REACT_APP_CRM_NAME}</p>
+        </Row>
       </div>
     );
   }
