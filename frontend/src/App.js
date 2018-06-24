c9414c56494ae5dcc0c1768c73cce1d061b83b07
Andi Andreas <nexuister@gmail.com>
2018-06-23 23:14:59 -0400

    Add google maps

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,8 +9,21 @@ import {
   SideNavItem,
   Button
 } from "react-materialize";
-
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker
+} from "react-google-maps";
 require("dotenv").config();
+
+const MapWithAMarker = withScriptjs(
+  withGoogleMap(props => (
+    <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
+      <Marker position={{ lat: -34.397, lng: 150.644 }} />
+    </GoogleMap>
+  ))
+);
 
 export default class App extends Component {
   render() {
@@ -42,6 +55,14 @@ export default class App extends Component {
         </SideNav>
         <Row style={{ paddingLeft: "320px" }}>
           <p>andi {process.env.REACT_APP_CRM_NAME}</p>
+          <MapWithAMarker
+            googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${
+              process.env.REACT_APP_GMAPS_API_KEY
+            }`}
+            loadingElement={<div style={{ height: `100%` }} />}
+            containerElement={<div style={{ height: `400px` }} />}
+            mapElement={<div style={{ height: `100%`, width: `400px` }} />}
+          />
         </Row>
       </div>
     );
